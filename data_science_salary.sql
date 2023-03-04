ef4a2adf65c2fede34096f972cfe14d7716a1f62
Pritam Shetty <109093067+PritamSShetty@users.noreply.github.com>
2023-03-04 14:57:10 +0530

    Add files via upload

--- data_science_salary.sql
+++ data_science_salary.sql
@@ -8,182 +8,214 @@ from ds_salaries;
 
 
 -- 1. How many job title was included in the dataset?
-select count(distinct(job_title)) as Count_of_distinct_Job_Title
-from ds_salaries;
+SELECT 
+    COUNT(DISTINCT (job_title)) AS Count_of_distinct_Job_Title
+FROM
+    ds_salaries;
 
 -- 2. Which experience level has the highest salary?
-update ds_salaries
-set experience_level = 
-case
-when experience_level = 'MI' then 'Middle Level'
-when experience_level = "SE" then 'Senior Level'
-when experience_level = 'EN' then 'Entry Level'
-when experience_level = 'EX' then 'Executive Level'
-else null
-end;
+UPDATE ds_salaries 
+SET 
+    experience_level = CASE
+        WHEN experience_level = 'MI' THEN 'Middle Level'
+        WHEN experience_level = 'SE' THEN 'Senior Level'
+        WHEN experience_level = 'EN' THEN 'Entry Level'
+        WHEN experience_level = 'EX' THEN 'Executive Level'
+        ELSE NULL
+    END;
 
 
-select experience_level, salary_in_usd
-from ds_salaries
-order by salary_in_usd desc
-limit 1;
+SELECT 
+    experience_level, salary_in_usd
+FROM
+    ds_salaries
+ORDER BY salary_in_usd DESC
+LIMIT 1;
 
 -- 3. Does the employment type affect salary?
 
-select distinct(employment_type)
-from ds_salaries;
+SELECT DISTINCT
+    (employment_type)
+FROM
+    ds_salaries;
 
-update ds_salaries
-set employment_type = 
-case
-when employment_type = 'FT' then 'Full Time'
-when employment_type = "CT" then 'Contract'
-when employment_type = 'PT' then 'Part Time'
-when employment_type = 'FL' then 'Freelance'
-else null
-end;
+UPDATE ds_salaries 
+SET 
+    employment_type = CASE
+        WHEN employment_type = 'FT' THEN 'Full Time'
+        WHEN employment_type = 'CT' THEN 'Contract'
+        WHEN employment_type = 'PT' THEN 'Part Time'
+        WHEN employment_type = 'FL' THEN 'Freelance'
+        ELSE NULL
+    END;
 
 
-with avg_salary as 
-(
-select employment_type, round(avg(salary_in_usd),0) as avg_salaries,
-rank() over(partition by employment_type ORDER BY avg(salary_in_usd) DESC) AS salary_rank
-from ds_salaries
-group by employment_type
-)
-select employment_type, avg_salaries
-from avg_salary
-where salary_rank = 1
-order by avg_salaries desc;
+WITH avg_salary
+     AS (SELECT employment_type,
+                Round(Avg(salary_in_usd), 0)          AS avg_salaries,
+                Rank()
+                  OVER(
+                    partition BY employment_type
+                    ORDER BY Avg(salary_in_usd) DESC) AS salary_rank
+         FROM   ds_salaries
+         GROUP  BY employment_type)
+SELECT employment_type,
+       avg_salaries
+FROM   avg_salary
+WHERE  salary_rank = 1
+ORDER  BY avg_salaries DESC;
 
 -- 4. What is the average salary per job title in USD?
-select job_title, round(avg(salary_in_usd),0) as avg_salary_in_USD
-from ds_salaries
-group by job_title
-order by avg(salary_in_usd) desc;
+SELECT job_title,
+       Round(Avg(salary_in_usd), 0) AS avg_salary_in_USD
+FROM   ds_salaries
+GROUP  BY job_title
+ORDER  BY Avg(salary_in_usd) DESC; 
 
 -- 5. What is the highest salary per job title in USD?
 
 -- solution 1
-with salary as
-(
-select job_title, salary_in_usd,
-rank() over(partition by job_title order by salary_in_usd desc) as salary_rank
-from ds_salaries
-)
-select job_title, salary_in_usd
-from salary
-where salary_rank = 1
-order by salary_in_usd desc;
+WITH salary
+     AS (SELECT job_title,
+                salary_in_usd,
+                Rank()
+                  OVER(
+                    partition BY job_title
+                    ORDER BY salary_in_usd DESC) AS salary_rank
+         FROM   ds_salaries)
+SELECT job_title,
+       salary_in_usd
+FROM   salary
+WHERE  salary_rank = 1
+ORDER  BY salary_in_usd DESC; 
 
 -- solution 2
-select job_title, max(salary_in_usd) as highest_salary
-from ds_salaries
-group by job_title
-order by max(salary_in_usd) desc;
+SELECT job_title,
+       Max(salary_in_usd) AS highest_salary
+FROM   ds_salaries
+GROUP  BY job_title
+ORDER  BY Max(salary_in_usd) DESC; 
 
 -- 6. What is the highest paying entry-level data science job?
-select job_title, max(salary_in_usd) as Highest_Salary
-from ds_salaries
-where experience_level = 'Entry Level'
-group by job_title
-order by max(salary_in_usd) desc
-limit 5;
+SELECT job_title,
+       Max(salary_in_usd) AS Highest_Salary
+FROM   ds_salaries
+WHERE  experience_level = 'Entry Level'
+GROUP  BY job_title
+ORDER  BY Max(salary_in_usd) DESC
+LIMIT  5; 
 
 -- 7. What is the highest paying mid-level data science job?
 
-select job_title, max(salary_in_usd) as Highest_salary
-from ds_salaries
-where experience_level  = 'Middle Level'
-group by job_title
-order by max(salary_in_usd) desc;
+SELECT job_title,
+       Max(salary_in_usd) AS Highest_salary
+FROM   ds_salaries
+WHERE  experience_level = 'Middle Level'
+GROUP  BY job_title
+ORDER  BY Max(salary_in_usd) DESC; 
 
 -- 8. What is the highest paying senior-level data science job?
 
-select job_title, max(salary_in_usd) as Highest_salary
-from ds_salaries
-where experience_level  = 'Senior Level'
-group by job_title
-order by max(salary_in_usd) desc;
+SELECT job_title,
+       Max(salary_in_usd) AS Highest_salary
+FROM   ds_salaries
+WHERE  experience_level = 'Senior Level'
+GROUP  BY job_title
+ORDER  BY Max(salary_in_usd) DESC; 
 
 -- 9. What is the highest paying Executive level data science job?
 
-select job_title, max(salary_in_usd) as Highest_salary
-from ds_salaries
-where experience_level  = 'Executive Level'
-group by job_title
-order by max(salary_in_usd) desc;
+SELECT job_title,
+       Max(salary_in_usd) AS Highest_salary
+FROM   ds_salaries
+WHERE  experience_level = 'Executive Level'
+GROUP  BY job_title
+ORDER  BY Max(salary_in_usd) DESC; 
 
 -- 10. What is the average salary per experience level?
 
-select experience_level, round(avg(salary_in_usd),2) as avg_salary
-from ds_salaries
-group by experience_level
-order by avg_salary desc;
+SELECT experience_level,
+       Round(Avg(salary_in_usd), 2) AS avg_salary
+FROM   ds_salaries
+GROUP  BY experience_level
+ORDER  BY avg_salary DESC; 
 
 -- 11. What is the lowest paying entry-level data science job?
 
-select job_title, min(salary_in_usd) as Min_Salary
-from ds_salaries
-where experience_level = 'Entry Level'
-group by job_title
-order by Min_Salary
-limit 1;
+SELECT job_title,
+       Min(salary_in_usd) AS Min_Salary
+FROM   ds_salaries
+WHERE  experience_level = 'Entry Level'
+GROUP  BY job_title
+ORDER  BY min_salary
+LIMIT  1; 
 
 -- 12. What is the lowest paying Mid-level Data Science Job?
 
-select job_title, min(salary_in_usd) as Min_Salary
-from ds_salaries
-where experience_level = 'Middle Level'
-group by job_title
-order by Min_Salary
-limit 1;
+SELECT job_title,
+       Min(salary_in_usd) AS Min_Salary
+FROM   ds_salaries
+WHERE  experience_level = 'Middle Level'
+GROUP  BY job_title
+ORDER  BY min_salary
+LIMIT  1; 
 
 -- 13. What is the lowest paying Senior level data science job?
 
-select job_title, min(salary_in_usd) as Min_Salary
-from ds_salaries
-where experience_level = 'Senior Level'
-group by job_title
-order by Min_Salary
-limit 1;
+SELECT job_title,
+       Min(salary_in_usd) AS Min_Salary
+FROM   ds_salaries
+WHERE  experience_level = 'Senior Level'
+GROUP  BY job_title
+ORDER  BY Min_salary
+LIMIT  1; 
+
 
 -- 14. What is the lowest paying Executive level data science job?
 
-select job_title, min(salary_in_usd) as Min_Salary
-from ds_salaries
-where experience_level = 'Executive Level'
-group by job_title
-order by Min_Salary
-limit 1;
+SELECT job_title,
+       Min(salary_in_usd) AS Min_Salary
+FROM   ds_salaries
+WHERE  experience_level = 'Executive Level'
+GROUP  BY job_title
+ORDER  BY min_salary
+LIMIT  1; 
 
 -- 15. Does company size affect salary?
 select distinct(company_size)
 from ds_salaries;
 
-update ds_salaries
-set company_size = 
-case
-when company_size = 'L' then 'Large Comapny'
-when company_size = "S" then 'Small Company'
-when company_size = 'M' then 'Medium Company'
-else null
-end;
+UPDATE ds_salaries 
+SET 
+    company_size = CASE
+        WHEN company_size = 'L' THEN 'Large Comapny'
+        WHEN company_size = 'S' THEN 'Small Company'
+        WHEN company_size = 'M' THEN 'Medium Company'
+        ELSE NULL
+    END;
 
 select distinct(company_size)
 from ds_salaries;
 
-select company_size, min(salary_in_usd) as Min_Salary, max(salary_in_usd) as Max_Salary
-from ds_salaries
-group by company_size
-order by company_size;
+SELECT 
+    company_size,
+    MIN(salary_in_usd) AS Min_Salary,
+    MAX(salary_in_usd) AS Max_Salary
+FROM
+    ds_salaries
+GROUP BY company_size
+ORDER BY company_size;
 
 -- 16. Does company location affect Salary?
 
-select company_location, min(salary_in_usd) as Min_Salary, max(salary_in_usd) as Max_Salary
-from ds_salaries
-group by company_location
-order by company_location;
+SELECT 
+    company_location,
+    MIN(salary_in_usd) AS Min_Salary,
+    MAX(salary_in_usd) AS Max_Salary
+FROM
+    ds_salaries
+GROUP BY company_location
+ORDER BY company_location;
 
 
 
